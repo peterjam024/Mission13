4fde0239d8f7f9a22e73c09180d182a91e708702
James Peterson <jamespetersonwrestling@gmail.com>
2022-04-01 00:28:08 -0600

    oofdies

---
+++ Views/Shared/Bowlers.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Mission13.Infrastructure;
+using Mission13.Models;
+
+namespace Mission13.Pages
+{
+    public class Bowlers : PageModel
+    {
+
+        private IBowlerRepository repo { get; set; }
+
+        public BowlerModel(IBowlerRepository temp, Bowler b)
+        {
+            repo = temp;
+            bowler = b;
+        }
+
+        public Bowler bowler { get; set; }
+        public string ReturnURL { get; set; }
+        public void OnGet(string returnURL)
+        {
+            ReturnURL = returnURL ?? "/";
+        }
+
+        public IActionResult OnPost(int bowlerID, string returnURL)
+        {
+            Bowler b = repo.Bowler.FirstOrDefault(x => x.BowlerID == bowlerID);
+
+            bowler.AddItem(b, 1);
+
+            return RedirectToPage(new { ReturnURL = returnURL });
+        }
+
+        public IActionResult OnPostRemove(int bowlerID, string returnUrl)
+        {
+            bowler.RemoveItem(bowler.Items.First(x => x.Bowler.BowlerID == bowlerID).Project);
+
+            return RedirectToPage(new { ReturnURL = returnUrl });
+        }
+
+    }
+}
